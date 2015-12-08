7e7b40626911406b2061a3f8014163e03217c256
ERiok <eriobackup@gmail.com>
2015-12-08 19:41:31 +0800

    add

---
+++ utils.js
@@ -0,0 +1,12 @@
+function getJsonData(url, callback){
+    var req = new XMLHttpRequest;
+    req.open("GET", url);
+    req.send();
+    req.onreadystatechange = function() {
+        var status = req.readyState;
+        if(status == 4){
+            callback(req.responseText);
+        }
+    }
+}
+
