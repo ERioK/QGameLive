c2fde78f9b1a20740952bd2a1b24ea4ae3b46b12
ERiok <eriobackup@gmail.com>
2016-01-06 15:22:19 +0800

    fix zhanqi

--- zhanqi.js
+++ zhanqi.js
@@ -34,18 +34,30 @@ function getRooms(md, cateid){
     Utils.getJsonData(cat_url, showRooms);
 }
 
+function getKey(cdn){
+    var MASK = "#{&..?!(";
+    var result = '';
+    console.log(cdn.length);
+    for(var i = 0; i < cdn.length; i++){
+        result += String.fromCharCode(MASK[i%8].charCodeAt(0) ^ cdn[i].charCodeAt(0));
+    }
+    return result;
+}
+
 function getUrl(roomId){
     var url_t = video_url1 + roomId + video_url2;
-    console.log(url_t);
     var videoOpen = function(data){
         var data = JSON.parse(data).data;
         var videoId = data.videoId;
-        var r_url = real_url1 + videoId + real_url2;
-        var furl = "rtmp://dlrtmp.cdn.zhanqi.tv/zqlive/" + videoId;
+        var cdns = data.flashvars.cdns;
+        var ak2 = JSON.parse(Qt.atob(cdns)).ak2;
+        var cdn = ak2.split("|")[0].split("-")[1];
+        var key = getKey(Qt.atob(cdn));
+        var timestr = parseInt(new Date().getTime()/1000 + 1).toString(16);
+        var fullkey = key + "/zqlive/" + videoId + timestr;
+        console.log(fullkey);
+        var furl = "rtmp://dlrtmp.cdn.zhanqi.tv/zqlive/" + videoId + "?k=" + Qt.md5(fullkey) + "&t=" + timestr;
         Qt.openUrlExternally(furl);
-        //Utils.getJsonData(r_url, function(furl){
-        //    Qt.openUrlExternally(furl);
-        //});
     };
     Utils.getJsonData(url_t, videoOpen);
 }
